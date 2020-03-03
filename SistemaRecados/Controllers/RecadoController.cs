4b62073d01ff9e2c7c423bbf88eeb3057e3dd3f2
daviradames <daviradames@gmail.com>
2020-03-03 20:45:13 -0300

    Versão inicial - Sistema Recados

---
+++ SistemaRecados/Controllers/RecadoController.cs
@@ -0,0 +1,97 @@
+﻿
+using SistemaRecados.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaRecados.Controllers
+{
+    public class RecadoController : Controller
+    {
+        private static long ProxId = 1;
+
+        private static IList<Recado> lista = new List<Recado>()
+        {
+            new Recado()
+            {
+                RecadoID = ProxId++,
+                Mensagem = "Subiu mural",
+                DataHora = DateTime.Now,
+                Autor = "Admin"
+            }
+        };
+
+        public RecadoController()
+        {
+            /*
+             Recado r = new Recado();
+            r.RecadoID = 1;
+            r.Mensagem = "Traz pão";
+            r.DataHora = DateTime.Now;
+            r.Autor = "Zé";
+            
+            lista.Add(r);
+
+            r = new Recado();
+            r.RecadoID = 2;
+            r.Mensagem = "Compra Mortandela?";
+            r.DataHora = DateTime.Now;
+            r.Autor = "Jão";
+
+            lista.Add(r);
+
+            lista.Add(new Recado()
+            {
+                RecadoID = 3,
+                Mensagem = "Quantas gramas?",
+                DataHora = DateTime.Now,
+                Autor = "Zé"
+            });
+
+            */
+        }
+
+        // GET: Recado
+        public ActionResult Index()
+        {
+            return View(lista);
+        }
+
+        public ActionResult GetDetalhe(int id)
+        {
+            Recado rec = lista.First<Recado>(r => r.RecadoID == id);
+            if (rec != null)
+            {
+                return View(rec);
+            }
+            return HttpNotFound();
+
+            /*
+            foreach (Recado rec in lista)
+            {
+                if (rec.RecadoID == id)
+                {
+                    return View(rec);
+                }
+            }
+
+            return HttpNotFound();
+            */
+        }
+    
+        public ActionResult NovoRecado()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NovoRecado(Recado recado)
+        {
+            recado.DataHora = DateTime.Now;
+            recado.RecadoID = ProxId++;
+            lista.Add(recado);
+            return View();
+        }
+    }
+}
---
+++ SistemaRecados/Models/Recado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaRecados.Models
+{
+    public class Recado
+    {
+        public long RecadoID { get; set; }
+        public string Mensagem { get; set; }
+        public DateTime DataHora { get; set; }
+        public string Autor { get; set; }
+        public string Destinatario { get; set; }
+    }
+}
